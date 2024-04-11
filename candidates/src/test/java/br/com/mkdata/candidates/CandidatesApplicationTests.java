203031ac44a49b7d29f6d314ea8e8f6b599953ed
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2024-04-11 10:37:30 -0400

    chore: add candidates, vacancies, gateway project

---
+++ candidates/src/test/java/br/com/mkdata/candidates/CandidatesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mkdata.candidates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ vacancies/src/test/java/br/com/mkdata/vacancies/VacanciesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mkdata.vacancies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacanciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
